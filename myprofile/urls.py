d2ac78f5d7c7c702a92fbe9369662643a6446d5b
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-06-27 16:55:12 +0530

    some changes

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('allprojects/',views.allprojects,name='allprojects'),
     path('allskills',views.allskills,name='allskills'),
+    path('/<int:pk>',views.singleproject,name='singleproject'),
     path('<int:pk>',views.singleskill,name='singleskill'),
-    path('<int:pk>',views.singleproject,name='singleproject'),
     path('contactme',views.contactme,name='contactme'),
     path('enqsuc',views.enqsuccess,name='enqsuc'),
 ]
